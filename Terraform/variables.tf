a92631183e9edf596f1a7bf5b4b7ed0852c2131b
Dhruv Mistry <dhruvmistry2000@gmail.com>
2025-04-19 11:49:52 +0530

    adding secrets for variables

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,18 +1,18 @@
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
-  default     = "ami-0fc5d935ebf8bc3bc" 
+  type        = string
 }
 
 variable "instance_type" {
-  description = "Instance type for the EC2 instance"
-  default     = "t2.medium" 
+  description = "Instance type for the EC2 instance" 
+  type        = string
 }
 
 variable "image_uri" {
   description = "URI of the Docker image to deploy"
-  default     = "dhruvmistry200/strapi-app:latest"
+  type        = string
 }
 variable "key_name" {
   description = "Key-pair name for SSH access to the instance"
-  default     = "starpi-app"
+  type        = string
 }

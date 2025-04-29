6f5a7190a524e65e16b007a47583de84b9f2ac4f
Dhruv Mistry <dhruvmistry2000@gmail.com>
2025-04-29 15:15:42 +0530

    Solving the errors

--- Terraform2/code-deploy.tf
+++ Terraform2/code-deploy.tf
@@ -1,5 +1,5 @@
 resource "aws_codedeploy_app" "strapi" {
-  name = "StrapiCodeDeployApp"
+  name             = "StrapiCodeDeployApp"
   compute_platform = "ECS"
 }
 
@@ -7,25 +7,23 @@ resource "aws_codedeploy_deployment_group" "strapi" {
   app_name              = aws_codedeploy_app.strapi.name
   deployment_group_name = "StrapiDeploymentGroup"
   service_role_arn      = aws_iam_role.codedeploy_service_role.arn
+  deployment_config_name = "CodeDeployDefault.ECSAllAtOnce"
 
   deployment_style {
-    deployment_type = "BLUE_GREEN"
+    deployment_type   = "BLUE_GREEN"
     deployment_option = "WITH_TRAFFIC_CONTROL"
   }
 
   blue_green_deployment_config {
     terminate_blue_instances_on_deployment_success {
-      action = "TERMINATE"
-      termination_wait_time_in_minutes = 5
+      action                             = "TERMINATE"
+      termination_wait_time_in_minutes  = 5
     }
 
     deployment_ready_option {
       action_on_timeout = "CONTINUE_DEPLOYMENT"
     }
 
-    green_fleet_provisioning_option {
-      action = "DISCOVER_EXISTING"
-    }
   }
 
   ecs_service {
@@ -36,14 +34,20 @@ resource "aws_codedeploy_deployment_group" "strapi" {
   load_balancer_info {
     target_group_pair_info {
       target_group {
-        name = aws_lb_target_group.strapi_tg_green.name
+        name = aws_lb_target_group.strapi_tg_blue.name
       }
       target_group {
         name = aws_lb_target_group.strapi_tg_green.name
       }
+
       prod_traffic_route {
         listener_arns = [aws_lb_listener.listener.arn]
       }
     }
+  }
+
+  auto_rollback_configuration {
+    enabled = true
+    events  = ["DEPLOYMENT_FAILURE"]
   }
 }

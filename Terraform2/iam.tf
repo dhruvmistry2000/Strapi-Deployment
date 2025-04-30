9994c57d3251fa51de65c1cec90878a602e9bb13
Dhruv Mistry <dhruvmistry2000@gmail.com>
2025-04-30 18:57:52 +0530

    changes

--- Terraform2/iam.tf
+++ Terraform2/iam.tf
@@ -22,23 +22,3 @@ resource "aws_iam_role" "ecs_task_execution_role" {
     }]
   })
 }
-
-resource "aws_iam_role" "codedeploy_service_role" {
-  name = "CodeDeployECSServiceRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Principal = {
-        Service = "codedeploy.amazonaws.com"
-      },
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "codedeploy_policy" {
-  role       = aws_iam_role.codedeploy_service_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AWSCodeDeployRoleForECS"
-}
